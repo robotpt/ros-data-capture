aab09c735c6f60cf189098c6665f69b019a9373d
Audrow <audrow.nash@gmail.com>
2020-02-21 11:04:20 -0800

    Create a way to upload files to AWS S3 storage

---
+++ aws_uploader/src/aws_uploader/aws_file_uploader.py
@@ -0,0 +1,102 @@
+import logging
+import boto3
+import os
+
+from botocore.exceptions import ClientError
+
+
+class AwsFileUploader:
+
+    def __init__(self, region, default_bucket=None):
+
+        self._default_region = region
+        self._s3_client = boto3.client('s3', region_name=self._default_region)
+
+        if default_bucket is None:
+            default_bucket = raw_input("What is the name of your bucket?\n>>> ")
+
+        self._create_bucket_if_doesnt_exist(default_bucket, self._default_region)
+        self._default_bucket = default_bucket
+
+    def upload_file(self, file_path, bucket_name=None, is_remove_file_on_success=False):
+
+        if bucket_name is None:
+            bucket_name = self._default_bucket
+
+        try:
+            self._s3_client.upload_file(file_path, bucket_name, os.path.basename(file_path))
+        except ClientError:
+            logging.error("Failed to upload '{}'".format(file_path))
+        else:
+            logging.info("Successfully uploaded '{}'".format(file_path))
+            if is_remove_file_on_success:
+                os.remove(file_path)
+                logging.info("Deleted file after successful upload: '{}'".format(file_path))
+
+    def get_bucket_names(self):
+        return [bucket[u'Name'] for bucket in self._s3_client.list_buckets()[u'Buckets']]
+
+    def set_default_bucket(self, name, region=None):
+
+        if region is None:
+            region = self._default_region
+
+        self._create_bucket_if_doesnt_exist(name, region)
+        self._default_bucket = name
+        logging.info("Default bucket set to '{}'".format(name))
+
+    def _create_bucket_if_doesnt_exist(self, name, region=None):
+
+        if self._is_bucket_exist(name):
+            logging.info("Bucket '{}' exists".format(name))
+        else:
+
+            if region is None:
+                region = self._default_region
+
+            logging.info("Bucket '{}' doesn't exist -- attempting to create".format(name))
+            self._create_bucket(
+                name=name,
+                region=region,
+            )
+            logging.info("Bucket '{}' created in region '{}'".format(name, region))
+
+    def _is_bucket_exist(self, name):
+        return name in self.get_bucket_names()
+
+    def _create_bucket(self, name, region=None):
+
+        if region is None:
+            region = self._default_region
+
+        location = {'LocationConstraint': region}
+        self._s3_client.create_bucket(
+            Bucket=name,
+            CreateBucketConfiguration=location,
+        )
+
+
+if __name__ == '__main__':
+
+    import datetime
+
+    temp_dir = 'temp'
+    temp_file = 'foo.bar'
+
+    bucket_name_ = "my-bucket-124354731409183749"
+    bucket_region_ = "us-west-1"
+
+    aws_uploader = AwsFileUploader(
+        region=bucket_region_,
+        default_bucket=bucket_name_,
+    )
+
+    if not os.path.exists(temp_dir):
+        os.mkdir(temp_dir)
+
+    file_path = os.path.join(temp_dir, temp_file)
+    with open(file_path, 'w') as f:
+        f.write("Some content generated at {}".format(datetime.datetime.now().strftime("%H:%M:%S on %Y-%m-%d")))
+
+    aws_uploader.upload_file(file_path, is_remove_file_on_success=True)
+    os.rmdir(temp_dir)
