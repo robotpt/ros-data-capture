e5e4fcba145e77b85ad87fa64130638acccc1d37
Audrow Nash <audrow.nash@gmail.com>
2020-09-09 09:38:42 -0700

    Publish a topic on delete a file
    Signed-off-by: Audrow Nash <audrow.nash@gmail.com>

--- src/tools/directory_size_manager/scripts/manager.py
+++ src/tools/directory_size_manager/scripts/manager.py
@@ -4,6 +4,8 @@ import rospy
 import sys
 import os
 
+from std_msgs.msg import String
+
 from directory_size_manager import DirectorySizeManager
 
 
@@ -13,11 +15,12 @@ DIR_IDX = 1
 def main():
 
     is_create_directory_if_doesnt_exist = rospy.get_param(
-        '/directory_size_manager/is_create_directory_if_doesnt_exist',
+        'directory_size_manager/is_create_directory_if_doesnt_exist',
     )
     max_size_in_bytes = 1024*rospy.get_param(
-        '/directory_size_manager/max_size_in_kilobytes',
+        'directory_size_manager/max_size_in_kilobytes',
     )
+    publish_topic = rospy.get_param('directory_size_manager/publish_topic')
 
     args = sys.argv
     directory_path = args[DIR_IDX]
@@ -34,12 +37,19 @@ def main():
         'directory_size_mananger_{}'.format(_get_directory_name_using_ros_char_set(directory_path)),
         anonymous=True,
     )
+    deleter_publisher = rospy.Publisher(publish_topic, String, queue_size=1)
 
     dsm = DirectorySizeManager(directory=directory_path, max_size_in_bytes=max_size_in_bytes)
 
     def directory_manager_callback(_):
         while dsm.is_too_large():
+
             rospy.loginfo("Deleting file '{}'".format(dsm.oldest_file))
+
+            msg = String()
+            msg.data = dsm.oldest_file
+            deleter_publisher.publish(msg)
+
             dsm.run_once()
 
     rospy.Timer(rospy.Duration(2), directory_manager_callback)
