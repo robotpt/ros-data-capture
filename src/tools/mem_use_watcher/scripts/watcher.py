fef96b1e74cbf58c569a3c7efab0261a83330e28
Anand Gokul <anandgokul18@gmail.com>
2021-03-31 23:32:49 -0700

    Fixed dir name and args for specifying mem limit

--- src/tools/mem_use_watcher/scripts/watcher.py
+++ src/tools/mem_use_watcher/scripts/watcher.py
@@ -3,6 +3,7 @@
 import rospy
 from std_msgs.msg import Bool
 import psutil
+import argparse
 
 
 def monitorAvailableMemory(memory_upperlimit_percent):
@@ -23,20 +24,29 @@ def monitorAvailableMemory(memory_upperlimit_percent):
     return False
 
 
-def mem_use_watcher():
+def mem_use_watcher(mem_upper_limit):
     pub = rospy.Publisher('is_memory_usage_exceeded', Bool, queue_size=10)
     rospy.init_node('mem_use_watcher', anonymous=True)
     rate = rospy.Rate(1)  # 1hz
     while not rospy.is_shutdown():
 
         # Check on free memory if exceeds 90% utilization
-        mem_usage = monitorAvailableMemory(memory_upperlimit_percent=90)
+        mem_usage = monitorAvailableMemory(
+            memory_upperlimit_percent=mem_upper_limit)
         pub.publish(mem_usage)
         rate.sleep()
 
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--mem-upper-limit', type=float, help='Memory utilization upper limit in percent',
+                        default=90.0)
+
+    args, _ = parser.parse_known_args()
+
     try:
-        mem_use_watcher()
+        mem_use_watcher(args.mem_upper_limit)
     except rospy.ROSInterruptException:
         pass
